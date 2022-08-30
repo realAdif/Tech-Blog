1e3938b52c243219efe1af123f66d9f11b41bd65
Aditya <adityagaikwad693@gmail.com>
2022-08-30 17:59:32 +1000

    style.css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,7 +8,11 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
 });
-router.get('/login',(req,res) =>{
-    res.render('/login');
+router.get('/login',async (req,res) =>{
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 module.exports = router;
