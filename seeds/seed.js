42cf5ecd0bfb208c8eb414e28e7411116d145a54
Aditya <adityagaikwad693@gmail.com>
2022-08-31 15:19:47 +1000

    Seed file

---
+++ seeds/seed.js
@@ -0,0 +1,17 @@
+const sequelize = require('../config/connection');
+
+const seedUsers = require('./userDate.json');
+const {User} = require('../models')
+
+const seedDataBase = async () =>{
+    await sequelize.sync({force: true});
+
+    const users = await User.bulkCreate(seedUsers, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+
+};
+seedDataBase();
