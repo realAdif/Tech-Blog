2e14908a0eceb0e5be788d2e79edab921cf1fdac
Aditya <adityagaikwad693@gmail.com>
2022-09-04 19:55:34 +1000

    comment

---
+++ controllers/api/comment.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const {Post} = require('../../models');
+
+router.post('/', async(req,res) => {
+    
+})
--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const dashboard = require('./dashboard');
 
+// router.use('/dashbord',dashboard);
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
