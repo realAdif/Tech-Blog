cdc16a14f4d9c750fbfa7560dc11e27de659aba1
Aditya <adityagaikwad693@gmail.com>
2022-09-04 20:06:39 +1000

    Post

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./users');
-
+const dashboardRoutes = require('./post');
 
 router.use('/users', userRoutes);
+router.use('/dashboard',dashboardRoutes);
 
 
 module.exports = router;
