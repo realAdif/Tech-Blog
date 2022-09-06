bc44035936d2fe97202b0b13391c56c1e408ded2
Aditya <adityagaikwad693@gmail.com>
2022-09-06 13:12:34 +1000

    comment

--- controllers/api/postComment.js
+++ controllers/api/postComment.js
@@ -2,15 +2,24 @@ const router = require('express').Router();
 const {Comment} = require('../../models');
 
 router.post('/', async(req,res)=>{
-    try{
-        const commentData = await Comment.create({
-            comment: req.body.comment
+    Comment.findAll({
+        where: {
+            user_id: req.session.user_id
+    
+        }
+    }).then(data =>{
+        const comment = data.map((comment)=>
+            comment.get({plain: true})
+        )
+
+        res.render('/comment:id', {
+            logged_in: req.session.logged_in,
+            comment
         });
 
-        res.status(400).json(commentData);
-    }catch(err){
+    }).catch(err =>{
         res.status(400).json(err);
-    }
+    });  
 });
 
 module.exports = router;
