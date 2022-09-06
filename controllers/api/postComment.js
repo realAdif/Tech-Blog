702472c243c57feb151e090f1cd611d29183edfd
Aditya <adityagaikwad693@gmail.com>
2022-09-06 16:50:08 +1000

    Posting Comment

--- controllers/api/postComment.js
+++ controllers/api/postComment.js
@@ -1,25 +1,14 @@
-const router = require('express').Router();
-const {Comment} = require('../../models');
+const router = require("express").Router();
+const { Comment } = require("../../models");
 
-router.post('/', async(req,res)=>{
-    Comment.findAll({
-        where: {
-            user_id: req.session.user_id
-    
-        }
-    }).then(data =>{
-        const comment = data.map((comment)=>
-            comment.get({plain: true})
-        )
-
-        res.render('/comment:id', {
-            logged_in: req.session.logged_in,
-            comment
-        });
-
-    }).catch(err =>{
-        res.status(400).json(err);
-    });  
+router.post("/", async (req, res) => {
+  Comment.create({ ...req.body, user_id: req.session.user_id })
+    .then((data) => {
+      const comment = data.map((comment) => comment.get({ plain: true }));
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- public/js/addComment.js
+++ public/js/addComment.js
@@ -2,8 +2,8 @@ const newComment = async (event)=>{
     event.preventDefault();
 
     const comment = document.querySelector('#comment_input').value;
-    
-    const response = await fetch('/api/comment/',{
+    console.log(comment);
+    const response = await fetch('api/comment/',{
         method: 'POST',
         body: JSON.stringify({comment}),
         headers:{
