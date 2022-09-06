f28ef9b526b36ee044203d7d1fb533c9f9ea25ba
Aditya <adityagaikwad693@gmail.com>
2022-09-06 21:18:56 +1000

    Post reload fixed

--- controllers/api/postComment.js
+++ controllers/api/postComment.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 const { Comment } = require("../../models");
 
 router.post("/", async (req, res) => {
+    console.log({ ...req.body, user_id: req.session.user_id })
   Comment.create({ ...req.body, user_id: req.session.user_id })
     .then((data) => {
-    //   const comment = data.map((comment) => comment.get({ plain: true }));
+    //   const comment = data.map((comment) => comment.get({ plain: true }));  
         console.log(data);
-        res.status(200)
+        const comment = data.get({plain: true});
+        res.status(200).json(comment);
     })
     .catch((err) => {
       console.log(err);
