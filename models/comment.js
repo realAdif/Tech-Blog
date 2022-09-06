2d397a79280e921f4303d9ac18fdb0e7c28a4e1b
Aditya <adityagaikwad693@gmail.com>
2022-09-07 00:52:49 +1000

    Fixed model

--- models/comment.js
+++ models/comment.js
@@ -5,6 +5,7 @@ class Comment extends Model{}
 
 Comment.init(
     {
+        
         user_id:{
             type:DataTypes.INTEGER,
             allowNull: false,
--- models/user.js
+++ models/user.js
@@ -4,8 +4,9 @@ const sequelize = require("../config/connection");
 
 class User extends Model{
   checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
+        return bcrypt.compareSync(loginPw, this.password);
+  
+    }
 }
 
 User.init(
